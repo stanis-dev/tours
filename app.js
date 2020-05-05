3bc2c91e30674c3a4714261615c0a6ae6c02d4c1
thebadteacher <stanis.samisco@gmail.com>
2020-05-06 00:27:45 +0200

    Save 1

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+//Current -> write data to db
+app.post('/api/v1/tours', (req, res) => {
+  const {
+    startLocation,
+    ratingsAverage,
+    ratingsTotal,
+    image,
+    name,
+    duration,
+  } = req.body;
+
+  const newRoute = {
+    startLocation,
+    ratingsAverage,
+    ratingsTotal,
+    image,
+    name,
+    duration,
+  };
+
+  const data = tours.push(JSON.stringify(newRoute));
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, data, (err) => {
+    if (err) throw err;
+    console.log('data has been written');
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log('App running on port: ' + PORT);
+});

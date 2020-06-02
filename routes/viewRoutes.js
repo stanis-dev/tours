74f6788f6c92a31870256a527a6aefcb97a104eb
thebadteacher <stanis.samisco@gmail.com>
2020-06-02 18:12:46 +0200

    Implemented Stripe messages

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,9 +8,11 @@ const {
   getMyTours,
 } = require('../controllers/viewController');
 const { isLoggedIn, protect } = require('../controllers/authController');
-const { createBookingCheckout } = require('../controllers/bookingController');
+const {} = require('../controllers/bookingController');
 
 const router = express.Router();
+
+router.use(alerts);
 
 // The rout that will be hit once the credit card is successfully charged, BUT with a query to pass info
 // ...see bookingController.js --> getCheckoutSession --> .success_url config of stripe

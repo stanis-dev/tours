62eec7aa6c9c2a8d382505f0654bd163b2d75412
thebadteacher <stanis.samisco@gmail.com>
2020-06-02 17:32:56 +0200

    Improved Stripe Implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const { webhookCheckout } = require('./controllers/bookingController');
 
 const app = express();
 
@@ -48,6 +49,8 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
+
+app.post('/webhook-checkout', express.raw(), webhookCheckout);
 
 //Body parser
 app.use(express.json({ limit: '10kb' }));
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -15,7 +15,7 @@ const router = express.Router();
 // The rout that will be hit once the credit card is successfully charged, BUT with a query to pass info
 // ...see bookingController.js --> getCheckoutSession --> .success_url config of stripe
 // And when it is hit with the query pre-defined, a booking entry will be added to DB
-router.get('/', createBookingCheckout, isLoggedIn, getOverview);
+router.get('/', isLoggedIn, getOverview);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/tour/:tourSlug', isLoggedIn, getTour);
 router.get('/me', protect, getAccount);
